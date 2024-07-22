5e28036e38482b920f4b8b5b01811fb9ff5306a0
regisalam123 <regisalam123@gmail.com>
2024-07-22 14:04:58 +0700

    models untuk input ke trx_logsheet0

---
+++ paperless/Data/Models/InputLogsheet0.cs
@@ -0,0 +1,15 @@
+﻿namespace paperless.Data.Models
+{
+    public class InputLogsheet0
+    {
+        public string? Id { get; set; }
+        public string? IdJam { get; set; }
+        public string? LogId { get; set; }
+        public string? LogIddescr { get; set; }
+
+       // public Pekerjaanck3[] ListCeklist { get; set; }
+        public string? Maker { get; set; }
+        public string? ItemUnitid { get; set; }
+        public string? Note { get; set; }
+    }
+}
