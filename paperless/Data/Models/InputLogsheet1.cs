bde2d4c5eb5f54a9645ed4a375a547690d456f9c
regisalam123 <regisalam123@gmail.com>
2024-07-22 14:05:52 +0700

    models untuk input ke trx_logsheet1

---
+++ paperless/Data/Models/InputLogsheet1.cs
@@ -0,0 +1,11 @@
+﻿namespace paperless.Data.Models
+{
+    public class InputLogsheet1
+    {
+        public string? ParentId { get; set; }
+        public int? Line { get; set; }
+        public string? Uraian { get; set; }
+        public string? Isi { get; set; }
+        public string? Keterangan { get; set; }
+    }
+}
