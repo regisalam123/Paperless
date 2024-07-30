88ccb9387e39ed5ff7b8893ef64fee9d8a4e2ba6
regisalam123 <regisalam123@gmail.com>
2024-07-30 11:37:57 +0700

    add models idlog,iditem

---
+++ paperless/Data/Models/Datalogsheet.cs
@@ -0,0 +1,13 @@
+﻿namespace paperless.Data.Models
+{
+    public class Datalogsheet
+    {
+        public string? Idtgl { get; set; }
+        public string? Idlog { get; set; }
+        public string? Iditem { get; set; }
+        public string? Idjam { get; set; }
+        
+
+
+    }
+}
