beecd541c64e33ccc55cfe52afd04bb15f34485b
regisalam123 <regisalam123@gmail.com>
2024-08-06 13:36:05 +0700

    add modell insert nya

--- paperless/Data/Models/ApprovalJob.cs
+++ paperless/Data/Models/ApprovalJob.cs
@@ -4,6 +4,9 @@
     {
         public string? Id { get; set; }
         public string? Approval { get; set; }
+        public string? Approvejobby { get; set; }
+        public string? Rejectjobby { get; set; }
+        public string? Notejob { get; set; }
 
     }
 }
