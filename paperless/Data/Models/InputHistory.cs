a66a790a2201d59a719df3b0b11f27747782b76e
regisalam123 <regisalam123@gmail.com>
2024-08-16 14:10:33 +0700

    add models input history

---
+++ paperless/Data/Models/InputHistory.cs
@@ -0,0 +1,14 @@
+﻿namespace paperless.Data.Models
+{
+    public class InputHistory
+    {
+        public string? Id { get; set; }
+        public string? ItemUnit { get; set; }
+        public string? Descr { get; set; }
+        public string? Jumlah { get; set; }
+        public string? Note { get; set; }
+        public string? Maker { get; set; }
+        public string? Updateby { get; set; }
+       
+    }
+}
