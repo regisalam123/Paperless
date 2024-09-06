a4aaec39c3b439226c08947c8e39580d627ff1d2
regisalam123 <regisalam123@gmail.com>
2024-09-06 13:03:48 +0700

    edit nama models

--- paperless/Data/Models/FormPekerjaanck2.cs
+++ paperless/Data/Models/FormPekerjaanck2.cs
@@ -10,10 +10,8 @@
         public string? foto3 { get; set; }
         public string? foto4 { get; set; }
         public string? foto5 { get; set; }
-        public string? ItemUnit { get; set; }
-
-
-
+        public string? ItemUnitid { get; set; }
+        public string? ItemUnitiddescr { get; set; }
 
         public FormPekerjaanck3[] Kondisi { get; set; }
     }
