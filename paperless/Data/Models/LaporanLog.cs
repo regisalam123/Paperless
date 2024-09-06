1fda0f090c74deb68e745ad4c8e45be905f96f2a
regisalam123 <regisalam123@gmail.com>
2024-09-06 13:04:29 +0700

    add model laporan logs

---
+++ paperless/Data/Models/LaporanLog.cs
@@ -0,0 +1,9 @@
+﻿namespace paperless.Data.Models
+{
+    public class LaporanLog
+    {
+        public string? Idtglaw { get; set; }
+        public string? Idtglak { get; set; }
+        public string? Idjam { get; set; }
+    }
+}
