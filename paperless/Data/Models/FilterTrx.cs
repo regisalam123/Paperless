473a36827dd5eee63b48b87ca02d0e248834108c
regisalam123 <regisalam123@gmail.com>
2024-09-23 09:08:44 +0700

    add models filter trx nya

---
+++ paperless/Data/Models/FilterTrx.cs
@@ -0,0 +1,12 @@
+﻿namespace paperless.Data.Models
+{
+    public class FilterTrx
+    {
+        public string? Tanggalaw { get; set; }
+
+        public string? Tanggalak { get; set; }
+
+        public string? PekDescr { get; set; }
+
+    }
+}
