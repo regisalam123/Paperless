f72ff540ab529ab9a0a5a704693ad5ce91ac1eec
regisalam123 <regisalam123@gmail.com>
2024-10-03 14:29:27 +0700

    add idlok,descrlok

--- paperless/Data/Models/FormPekerjaanck2u.cs
+++ paperless/Data/Models/FormPekerjaanck2u.cs
@@ -11,6 +11,13 @@
         public string? foto4 { get; set; }
         public string? foto5 { get; set; }
         public string? Updateby { get; set; }
+        public string? ItemUnitid { get; set; }
+        public string? ItemUnitiddescr { get; set; }
+        public string? Idlok { get; set; }
+        public string? Descrlok { get; set; }
+
+
+
 
 
 
