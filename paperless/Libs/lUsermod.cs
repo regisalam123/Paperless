59fcbb8c37e0c9348766ea975c0a1502bf8bcf71
regisalam123 <regisalam123@gmail.com>
2024-10-15 11:42:04 +0700

    library untuk users MOD

---
+++ paperless/Libs/lUsermod.cs
@@ -0,0 +1,28 @@
+﻿using paperless.Data.Controllers;
+using Npgsql;
+using System.Data;
+using paperless.Data.Controllers;
+
+namespace paperless.Libs
+{
+    public class lUsermod
+    {
+        private lDbConn dbconn = new lDbConn();
+        private BaseController bc = new BaseController();
+
+
+        internal List<dynamic> ReadUser2(String iduser, String idpwd)
+        {
+            var cstrname = dbconn.constringName("idccore");
+            var split = "||";
+            var schema = "public";
+
+            string spname = "getusermod1";
+            string p1 = "@iduser" + split + iduser + split + "s";
+            string p2 = "@idpwd" + split + idpwd + split + "s";
+
+            return bc.ExecSqlWithReturnCustomSplit(cstrname, split, schema, spname, p1, p2);
+        }
+
+    }
+}
