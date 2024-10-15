91307f00e75e876cb141602ff8e801b3dea1a4af
regisalam123 <regisalam123@gmail.com>
2024-10-15 11:36:51 +0700

    add model untuk filtering pekerjaan MOD

---
+++ paperless/Data/Models/Filterpekerjaanmod.cs
@@ -0,0 +1,9 @@
+﻿namespace paperless.Data.Models
+{
+    public class Filterpekerjaanmod
+    {
+        public string? Tanggalaw { get; set; }
+
+        public string? Tanggalak { get; set; }
+    }
+}
