f8262c383578f69c2709c9f2c93582cd9946533b
regisalam123 <regisalam123@gmail.com>
2024-10-15 11:38:28 +0700

    add model untuk input ke modtrx0

---
+++ paperless/Data/Models/InputMod0.cs
@@ -0,0 +1,14 @@
+﻿namespace paperless.Data.Models
+{
+    public class InputMod0
+    {
+        public string? Idm { get; set; }
+        public string? Itemid0 { get; set; }
+        public string? Unit { get; set; }
+        public string? Lantai { get; set; }
+        public string? CreateBy { get; set; }
+        public string? UpdateBy { get; set; }
+        public string? Note { get; set; }
+        public InputMod1 [] Listmod1 { get; set; }
+    }
+}
