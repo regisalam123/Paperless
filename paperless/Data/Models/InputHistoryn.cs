0880e0dafdaa860d5495338e696636f336ad5029
regisalam123 <regisalam123@gmail.com>
2024-10-15 11:37:50 +0700

    add model untuk get history karena dulu error bikin model baru

---
+++ paperless/Data/Models/InputHistoryn.cs
@@ -0,0 +1,14 @@
+﻿namespace paperless.Data.Models
+{
+    public class InputHistoryn
+    {
+        public string? Id { get; set; }
+        public string? ItemUnit { get; set; }
+        public string? ItemDescr { get; set; }
+        public string? Jumlah { get; set; }
+        public string? Note { get; set; }
+        public string? Maker { get; set; }
+        public string? Approval { get; set; }
+        public string? Descr { get; set; }
+    }
+}
