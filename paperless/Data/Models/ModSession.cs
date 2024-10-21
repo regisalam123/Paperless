c207c6776ac99cb9ba63aec2c2014e93274f967a
regisalam123 <regisalam123@gmail.com>
2024-10-21 14:19:44 +0700

    add models mod session

---
+++ paperless/Data/Models/ModSession.cs
@@ -0,0 +1,11 @@
+﻿namespace paperless.Data.Models
+{
+    public class ModSession
+    {
+        public string? UserId { get; set; }
+        public string? Nama { get; set; }
+        public string? Peranan { get; set; }
+        public string? Eticket { get; set; }
+        public string? Status { get; set; }
+    }
+}
