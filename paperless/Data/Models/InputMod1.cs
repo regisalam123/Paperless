2e3e9fcc4a9e30523bc851013c20eaf5c33b3e7b
regisalam123 <regisalam123@gmail.com>
2024-10-21 14:18:15 +0700

    change models

--- paperless/Data/Models/InputMod1.cs
+++ paperless/Data/Models/InputMod1.cs
@@ -4,7 +4,7 @@
     {
         public string? Itemid1 { get; set; }
         public string? Itemid1Descr { get; set; }
-        public string? Note1 { get; set; }
-        public string? Deskripsi { get; set; }
+        public string? Keadaan { get; set; }
+        public string? Catatan { get; set; }
     }
 }
