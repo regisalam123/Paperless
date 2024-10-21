41c724c66e0f47c2c54306847cc63001365fcef9
regisalam123 <regisalam123@gmail.com>
2024-10-21 14:20:58 +0700

    add models mod update

---
+++ paperless/Data/Models/ModUpdate0.cs
@@ -0,0 +1,16 @@
+﻿namespace paperless.Data.Models
+{
+    public class ModUpdate0
+    {
+        public string? Idm { get; set; }
+        public string? Itemid0 { get; set; }
+        public string? Unit { get; set; }
+        public string? Lantai { get; set; }
+        public string? UpdateBy { get; set; }
+        public string? Note { get; set; }
+        public string? Itemid1 { get; set; }
+        public string? Itemid1Descr { get; set; }
+        public string? Keadaan { get; set; }
+        public string? Catatan { get; set; }
+    }
+}
