6faed0ffe9b9dc8261e4d452f6e1a04598e30b83
regisalam123 <regisalam123@gmail.com>
2024-11-01 14:50:45 +0700

    add 1 rows status

--- paperless/Data/Models/InputMod0.cs
+++ paperless/Data/Models/InputMod0.cs
@@ -9,6 +9,8 @@
         public string? CreateBy { get; set; }
         public string? UpdateBy { get; set; }
         public string? Note { get; set; }
+        public string? Status { get; set; }
+
         public InputMod1 [] Listmod1 { get; set; }
     }
 }
