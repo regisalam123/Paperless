e171a1a49ee223b503fc51309f1b7c79aaceec21
regisalam123 <regisalam123@gmail.com>
2024-11-01 14:52:32 +0700

    jadi 2 models

--- paperless/Data/Models/ModUpdate0.cs
+++ paperless/Data/Models/ModUpdate0.cs
@@ -3,14 +3,7 @@
     public class ModUpdate0
     {
         public string? Idm { get; set; }
-        public string? Itemid0 { get; set; }
-        public string? Unit { get; set; }
-        public string? Lantai { get; set; }
-        public string? UpdateBy { get; set; }
-        public string? Note { get; set; }
-        public string? Itemid1 { get; set; }
-        public string? Itemid1Descr { get; set; }
-        public string? Keadaan { get; set; }
-        public string? Catatan { get; set; }
+        public string? Status { get; set; }
+
     }
 }
