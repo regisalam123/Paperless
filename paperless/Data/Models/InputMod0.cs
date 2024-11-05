ee3793426958fba67c8c5e2607892a37d7d50dba
regisalam123 <regisalam123@gmail.com>
2024-11-05 11:28:31 +0700

    add descr

--- paperless/Data/Models/InputMod0.cs
+++ paperless/Data/Models/InputMod0.cs
@@ -4,12 +4,15 @@
     {
         public string? Idm { get; set; }
         public string? Itemid0 { get; set; }
+        public string? Itemid0descr { get; set; }
         public string? Unit { get; set; }
         public string? Lantai { get; set; }
         public string? CreateBy { get; set; }
         public string? UpdateBy { get; set; }
         public string? Note { get; set; }
         public string? Status { get; set; }
+        public string? TipeId { get; set; }
+        public string? TipeIddescr { get; set; }
 
         public InputMod1 [] Listmod1 { get; set; }
     }
